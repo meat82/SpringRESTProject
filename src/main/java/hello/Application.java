cfab8fdd19f87f9a35c9a78c824ad9bf50e20489
Mikko Torvinen <mikko.torvinen@ideal.fi>
2016-06-09 13:01:13 +0300

    It's alive!

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,10 +1,17 @@
 package hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,9 +26,18 @@ public class Application implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("Content-Type", "application/json");
+        requestHeaders.set("Authorization", "Token token=93de1ea75063ca3c270e17cc11d97897");
+        requestHeaders.set("Accept", "application/vnd.stattleship.com; version=1");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", requestHeaders);
         RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        log.info(quote.toString());        
+        //Quote quote = restTemplate.getForObject("https://www.stattleship.com/hockey/nhl/players", Quote.class);
+        Map<String, String> urlVariables = new HashMap<String,String>();
+        urlVariables.put("team_id", "nhl-bos");
+        //Quote quote = restTemplate.getForObject("https://www.stattleship.com/hockey/nhl/players", Quote.class, urlVariables);
+        ResponseEntity<String> response = restTemplate.exchange("https://www.stattleship.com/hockey/nhl/players?team_id=nhl-bos", HttpMethod.GET, entity, String.class);
+        log.info(response.toString());        
     }
     
 
