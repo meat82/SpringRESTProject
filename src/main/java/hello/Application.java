2e973fb13343adee1ce41a7d6939fa314070a760
Mikko Torvinen <mikko.torvinen82@gmail.com>
2016-06-14 21:10:30 +0300

    add command-line argument

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -14,41 +14,54 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    private static final String TOKEN_ID = "93de1ea75063ca3c270e17cc11d97897";
-    private static final String CONTENT_TYPE = "application/json";
-    private static final String ACCEPT = "application/vnd.stattleship.com; version=1";
-    private static final String URL = "https://www.stattleship.com/hockey/nhl/players";
-    private static final String SELECT_TEAM = "nhl-bos";
+	private static final String TOKEN_ID = "93de1ea75063ca3c270e17cc11d97897";
+	private static final String CONTENT_TYPE = "application/json";
+	private static final String ACCEPT = "application/vnd.stattleship.com; version=1";
+	private static final String URL = "https://www.stattleship.com/hockey/nhl/players";
+	private static final String SELECT_TEAM = "nhl-bos";
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 
-    }
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        HttpHeaders requestHeaders = new HttpHeaders();
-        StringBuilder url_builder = new StringBuilder(URL);
-        url_builder.append("?").append("team_id").append("=").append(SELECT_TEAM);
-        
-        requestHeaders.set(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
-        requestHeaders.set(HttpHeaders.AUTHORIZATION, TOKEN_ID);
-        requestHeaders.set(HttpHeaders.ACCEPT, ACCEPT);
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", requestHeaders);
+	@Override
+	public void run(String... args) throws Exception {
+		
+		if (args != null && args.length > 0) {
+			for (String arg : args) {
+				log.info("Display team: " + arg);
+				getPlayers(arg);
+			}
+		}
+		else {
+			getPlayers(SELECT_TEAM);
+		}
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Players> response = restTemplate.exchange(
-                url_builder.toString(), HttpMethod.GET, entity,
-                Players.class);
-        Players players = response.getBody();
-        
-        //Just loop through player from selected team
-        for (Player player : players.getPlayers()) {
-            log.info(player.toString());
-        }
+	}
 
-    }
+	private void getPlayers(String selectTeam) {
+		HttpHeaders requestHeaders = new HttpHeaders();
+		StringBuilder url_builder = new StringBuilder(URL);
+		url_builder.append("?").append("team_id").append("=").append(selectTeam);
+
+		requestHeaders.set(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
+		requestHeaders.set(HttpHeaders.AUTHORIZATION, TOKEN_ID);
+		requestHeaders.set(HttpHeaders.ACCEPT, ACCEPT);
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", requestHeaders);
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Players> response = restTemplate.exchange(url_builder.toString(), HttpMethod.GET, entity,
+				Players.class);
+		Players players = response.getBody();
+
+		// Just loop through player from selected team
+		for (Player player : players.getPlayers()) {
+			log.info(player.toString());
+		}
+		
+	}
 
 }
