1ef1fdadf7b2a6267c8aeb28fe2b0c89697dcaf6
Mikko Torvinen <mikko.torvinen@ideal.fi>
2016-06-17 12:17:40 +0300

    Change to web application

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,22 +3,27 @@ package hello;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class Application implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-	private static final String SELECT_TEAM = "nhl-bos";
     private Properties properties;
     private InputStream input;
+    private String securityToken;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -29,32 +34,21 @@ public class Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 	    
 		init(args);
-		String securityToken = properties.getProperty("key");
+		securityToken = properties.getProperty("key");
 		log.info("User token: " + securityToken);
-		PlayersByTeam pbt = new PlayersByTeam(securityToken);
-		if (args != null && args.length > 0) {
-			for (String arg : args) {
-				log.info("Display team: " + arg);
-				pbt.getPlayerByTeam(arg);
-				displayRESTData(pbt);
-			}
-		}
-		else {
-		    log.info("Display team: " + SELECT_TEAM);
-            pbt.getPlayerByTeam(SELECT_TEAM);
-            displayRESTData(pbt);
-		}
 
 	}
-
-	private void displayRESTData(AbstractREST data) {
-	    data.displayData();
-    }
-
+	
     private void init(String[] args) throws IOException {
         properties = new Properties();
         input = new FileInputStream("security.key");
         properties.load(input);
+    }
+    
+    @RequestMapping("/show")  
+    public List<Player> showData(@RequestParam(value="team", defaultValue="nhl-car") String selectTeam) {
+        PlayersByTeam pbt = new PlayersByTeam(securityToken);
+        return pbt.getPlayerByTeam(selectTeam);
     }
 
 }
